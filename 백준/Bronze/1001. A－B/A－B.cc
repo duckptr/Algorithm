e274d49ceda9c0a5afbaa362777b0bec353a0846
MyeongJun Kim <117059083+duckptr@users.noreply.github.com>
2026-01-26 13:18:35 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void) 
+{
+    int val1;
+    cout<<"";
+    cin>>val1;
+    
+    int val2;
+    cout<<"";
+    cin>>val2;
+    
+    int result = val1-val2;
+    cout<<""<<result<<endl;
+    return 0;
+}
