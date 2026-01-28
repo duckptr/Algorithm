8878a74910302b629c2b851313bf67eb3337752d
MyeongJun Kim <117059083+duckptr@users.noreply.github.com>
2026-01-28 09:54:13 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void) 
+{
+    int value1;
+    cout<<"";
+    cin>>value1;
+    
+    int result = value1 - 543;
+    cout<<""<<result<<endl;
+    return 0;
+}
